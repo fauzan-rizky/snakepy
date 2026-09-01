fb7447ca906e1814a3a6946cb2e62b5c8d83cbc6
fauzan-rizky <fauzan.rizky.naufal@gmail.com>
2026-09-01 18:51:24 +0700

    final V1

--- snake.py
+++ snake.py
@@ -1,73 +1,38 @@
 import os
+import sys
 import time
 import copy
-import subprocess
 import threading
 import readchar
+import random
 from readchar import key
 
 term_size = os.get_terminal_size()
 term_columns = term_size.columns
 term_lines = term_size.lines
-term_lines -= 1
-term_columns -= 1
-snake_length = 50
+term_lines -= 0
+term_columns -= 0
+snake_length = 10
 spawn_point = [int(round(term_lines) / 2), int(round(term_columns) / 2)+snake_length]  # Y, X
 terminal = []
 path = []
 direction = "none"
+
+apple_location = []
+apple_exists = False
+
 for i in range(term_lines):
         terminal.append([])
         for j in range(term_columns):
             terminal[i].append(" ")
+
 for i in range(snake_length):
         if i == 0:
             terminal[spawn_point[0]][spawn_point[1] - i] = ">" 
         else:
             terminal[spawn_point[0]][spawn_point[1] - i] = "#"
         path.append([int(round(term_lines) / 2), int(round(term_columns) / 2) - i])
-def update():
-    
-    terminal = []
-    for i in range(term_lines):
-        terminal.append([])
-        for j in range(term_columns):
-            terminal[i].append(" ")
-    past_path = copy.deepcopy(path)
-    if direction == "right":
-        if path[0][1] + 1 >= term_columns:
-            path[0][1] = 0
-        else:
-            path[0][1] += 1
-    if direction == "down":
-        if path[0][0] + 1 >= term_lines:
-            path[0][0] = 0
-        else:
-            path[0][0] += 1
-    if direction == "left":
-        if path[0][1] - 1 < 0:           
-            path[0][1] = term_columns - 1  
-        else:
-            path[0][1] -= 1
-    if direction == "up":
-        if path[0][0] - 1 < 0:           
-            path[0][0] = term_lines - 1  
-        else:
-            path[0][0] -= 1
-    for i in range(snake_length):  
-        if i == 0:
-            pass
-        else: 
-            path[i] = past_path[(i-1)]
-    for i in range(snake_length):
-        if i == 0:
-            terminal[path[i][0]][path[i][1]] = "█" 
-        else:
-            terminal[path[i][0]][path[i][1]] = "█"        
-    for x in range(term_lines):
-        print(*terminal[x], sep="")
-frames = 0
-direction = "right"
+
 def listen_input():
     global direction
     while True:
@@ -80,6 +45,106 @@ def listen_input():
             direction = "left"
         elif k == key.RIGHT and direction != "left":
             direction = "right"
+        elif k == key.CTRL_E:
+            sys.exit()
+
+def generate_apple():
+    global terminal, apple_exists, apple_location
+    if apple_location == [] and apple_exists == False:
+        apple_location = [random.randint(5, term_lines-5), random.randint(5,term_columns-5)]
+        apple_exists = True
+    else:
+        pass
+
+def check_meal(head):
+    global apple_location, apple_exists, snake_length, path
+    if head == apple_location:
+        apple_exists = False
+        apple_location = []
+        snake_length += 1
+        path.append([0,0])
+
+def check_collision(target, checkpath):
+    if target in checkpath:
+        game_over()
+
+def render_terminal():
+    global terminal, term_lines, term_columns
+    terminal = []
+    for i in range(term_lines):
+        terminal.append([])
+        for j in range(term_columns):
+            terminal[i].append(" ")
+
+def draw_border():
+    global terminal, term_lines, term_columns
+    for i in range(term_lines):
+        if i == 2 or i == term_lines-1:
+            for j in range(term_columns):
+                terminal[i][j] = "█"
+        else:
+            terminal[i][0] = "█"
+            terminal[i][(term_columns-1)] = "█"
+    
+def update():
+    
+    render_terminal()
+
+    past_path = copy.deepcopy(path)
+
+    if direction == "right":
+        if path[0][1] + 1 >= term_columns:
+            game_over()
+        else:
+            path[0][1] += 1
+            check_collision(path[0], past_path)
+    if direction == "down":
+        if path[0][0] + 1 >= term_lines:
+            game_over()
+        else:
+            path[0][0] += 1
+            check_collision(path[0], past_path)
+    if direction == "left":
+        if path[0][1] - 1 < 0:           
+            game_over()
+        else:
+            path[0][1] -= 1
+            check_collision(path[0], past_path)
+    if direction == "up":
+        if path[0][0] - 1 < 0:           
+            game_over() 
+        else:
+            path[0][0] -= 1
+            check_collision(path[0], past_path)
+
+    for i in range(snake_length):  
+        if i == 0:
+            pass
+        else: 
+            path[i] = past_path[(i-1)]
+
+    for i in range(snake_length):
+        if i == 0:
+            terminal[path[i][0]][path[i][1]] = "#" 
+        else:
+            terminal[path[i][0]][path[i][1]] = "#" 
+
+    generate_apple()
+    terminal[apple_location[0]][apple_location[1]] = "Q"
+    draw_border()
+ 
+    for x in range(term_lines):
+        print(*terminal[x], sep="")
+
+def game_over():
+    print("\033[2J")
+    print("GAME OVER!")
+    print(f"Score: {snake_length-10}")
+    sys.exit()
+
+frames = 0
+direction = "right"
+
 input_thread = threading.Thread(target=listen_input, daemon=True)
 input_thread.start()
 
@@ -89,10 +154,10 @@ while True:
     frames += 1
     
     update()
-    print(f"score: {snake_length}")
-    if frames%5 == 0:
-        snake_length+=1
-        path.append([0,0])
+    check_meal(path[0])
+    score_counter = f"score: {snake_length-10}"
+    margins = [" " for x in range((term_columns//2)-len(score_counter)//2)]
+    print(f"{"".join(margins)}score: {snake_length-10}")
     
-    time.sleep(1/24)
+    time.sleep(1/15)
     print("\033[2J")
