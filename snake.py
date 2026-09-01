96fef1bdc82666c256478d739cb7ff40e826f5ae
fauzan-rizky <fauzan.rizky.naufal@gmail.com>
2026-09-01 23:33:02 +0700

    migrated rendering to blessed

--- snake.py
+++ snake.py
@@ -2,10 +2,9 @@ import os
 import sys
 import time
 import copy
-import threading
-import readchar
 import random
 from readchar import key
+from blessed import Terminal
 
 term_size = os.get_terminal_size()
 term_columns = term_size.columns
@@ -18,6 +17,8 @@ terminal = []
 path = []
 direction = "none"
 
+term = Terminal()
+
 apple_location = []
 apple_exists = False
 
@@ -28,25 +29,24 @@ for i in range(term_lines):
 
 for i in range(snake_length):
         if i == 0:
-            terminal[spawn_point[0]][spawn_point[1] - i] = ">" 
+            terminal[spawn_point[0]][spawn_point[1] - i] = "#" 
         else:
             terminal[spawn_point[0]][spawn_point[1] - i] = "#"
         path.append([int(round(term_lines) / 2), int(round(term_columns) / 2) - i])
 
 def listen_input():
     global direction
-    while True:
-        k = readchar.readkey()
-        if k == key.UP and direction != "down":
-            direction = "up"
-        elif k == key.DOWN and direction != "up":
-            direction = "down"
-        elif k == key.LEFT and direction != "right":
-            direction = "left"
-        elif k == key.RIGHT and direction != "left":
-            direction = "right"
-        elif k == key.CTRL_E:
-            sys.exit()
+    val = term.inkey(timeout=0.001)
+    if val.name == "KEY_UP" and direction != "down":
+        direction = "up"
+    elif val.name == "KEY_DOWN" and direction != "up":
+        direction = "down"
+    elif val.name == "KEY_LEFT" and direction != "right":
+        direction = "left"
+    elif val.name == "KEY_RIGHT" and direction != "left":
+        direction = "right"
+    elif val.lower() == 'q':
+        game_over()
 
 def generate_apple():
     global terminal, apple_exists, apple_location
@@ -77,19 +77,30 @@ def render_terminal():
             terminal[i].append(" ")
 
 def draw_border():
-    global terminal, term_lines, term_columns
+    
+    global term, term_columns, term_lines
+    print(term.clear())
+    bars = ["█" for i in range(term_columns)]
     for i in range(term_lines):
-        if i == 2 or i == term_lines-1:
-            for j in range(term_columns):
-                terminal[i][j] = "█"
-        else:
-            terminal[i][0] = "█"
-            terminal[i][(term_columns-1)] = "█"
+        if i == 0 or i == term_lines - 1:
+            with term.location(x=0,y=i):
+                print("".join(bars), end="")
+        else:    
+            with term.location(x=0, y=i):
+                print("█", end="")
+            with term.location(x=term_columns,y=i):
+                print("█", end="")
+
+def draw_snake():
+    global path
+    for entry in path:
+        with term.location(entry[1], entry[0]):
+            print("#")
     
 def update():
-    
-    render_terminal()
 
+    render_terminal()
+    
     past_path = copy.deepcopy(path)
 
     if direction == "right":
@@ -131,13 +142,15 @@ def update():
 
     generate_apple()
     terminal[apple_location[0]][apple_location[1]] = "Q"
+    
     draw_border()
- 
-    for x in range(term_lines):
-        print(*terminal[x], sep="")
+    draw_snake()
 
+    with term.location(apple_location[1],apple_location[0]):
+            print("Q")
+ 
 def game_over():
-    print("\033[2J")
+    print(term.clear())
     print("GAME OVER!")
     print(f"Score: {snake_length-10}")
     sys.exit()
@@ -145,19 +158,18 @@ def game_over():
 frames = 0
 direction = "right"
 
-input_thread = threading.Thread(target=listen_input, daemon=True)
-input_thread.start()
-
 frames = 1
-while True:
 
-    frames += 1
-    
-    update()
-    check_meal(path[0])
-    score_counter = f"score: {snake_length-10}"
-    margins = [" " for x in range((term_columns//2)-len(score_counter)//2)]
-    print(f"{"".join(margins)}score: {snake_length-10}")
-    
-    time.sleep(1/15)
-    print("\033[2J")
+with term.hidden_cursor(), term.cbreak():
+    while True:
+
+        listen_input()
+
+        update()
+
+        check_meal(path[0])
+        score_counter = f"score: {snake_length-10}"
+        margins = [" " for x in range((term_columns//2)-len(score_counter)//2)]
+
+
+        time.sleep(1/10)
